3d0b5939f1e10d6504faeea92ecb9483fb69fa98
Micls <cptprice123@outlook.com>
2020-10-30 16:59:14 +0700

    [Micls] Update

--- LCD/Debug/TMRpcm/pcmRF.cpp.d
+++ LCD/Debug/TMRpcm/pcmRF.cpp.d
@@ -1,6 +1,7 @@
-C:\Users\Mikakka\AppData\Local\Temp\VMBuilds\LCD\mega_atmega2560\Debug\LCD.cpp.o: \
- C:\Users\Mikakka\AppData\Local\Temp\VMBuilds\LCD\mega_atmega2560\Debug\LCD.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+C:\Users\Mikakka\AppData\Local\Temp\VMBuilds\LCD\mega_atmega2560\Debug\TMRpcm\pcmRF.cpp.o: \
+ C:\Users\Mikakka\Documents\Arduino\libraries\TMRpcm\pcmRF.cpp \
+ C:\Users\Mikakka\Documents\Arduino\libraries\TMRpcm/pcmRF.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
@@ -11,12 +12,8 @@ C:\Users\Mikakka\AppData\Local\Temp\VMBuilds\LCD\mega_atmega2560\Debug\LCD.cpp.o
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\professional\common7\ide\extensions\0jpe4kch.yzv\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h \
  C:\Users\Mikakka\Documents\Arduino\libraries\TMRpcm/TMRpcm.h \
  C:\Users\Mikakka\Documents\Arduino\libraries\TMRpcm/pcmConfig.h \
- C:\Users\Mikakka\Documents\Arduino\libraries\TMRpcm/pcmRF.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/SD.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFat.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2Card.h \
@@ -24,8 +21,4 @@ C:\Users\Mikakka\AppData\Local\Temp\VMBuilds\LCD\mega_atmega2560\Debug\LCD.cpp.o
  C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdInfo.h \
  C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/FatStructs.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
- C:\Users\Mikakka\Documents\Arduino\libraries\LiquidCrystal_I2C-1.1.2/LiquidCrystal_I2C.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h
--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -6,19 +6,15 @@
 
 // the setup function runs once when you press reset or power the board
 
+#include <EEPROM.h>
+#include <TMRpcm.h>
+#include <pcmRF.h>
+#include <pcmConfig.h>
+#include <SPI.h>
+#include <SD.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
-#include<string.h>
-
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-
-#define Button1Pin 2	//Reaction button
-#define Button2Pin 3	//Enter button
-#define maxquestion 3	//How much question
-#define maxreaction 6
-
-#define locateXReactionStar 1		//Where is star appear
-#define locateYReactionStar 0		//Where is star from
+#include <string.h>
 
 enum state
 {
@@ -27,20 +23,75 @@ enum state
 	ENDREACTION = 2
 };
 
+typedef struct QandA
+{
+	char question[20];
+	uint8_t point;
+}maindata;
+
+#define Button1Pin 2				//Reaction button
+#define Button2Pin 3				//Enter button
+#define maxquestion 3				//How much question
+#define maxreaction 6				//How much point
+
+#define SD_ChipSelectPin 10			//SD pin config
+#define Speaker_output 9			//Speaker pin config
+#define Speaker_output_volumn 5		//Speaker volumn config
+
+#define locateXReactionStar 1		//Where is star appear
+#define locateYReactionStar 0		//Where is star from
+
+#define xlongLCD 16					//Length of LCD
+#define ylongLCD 2					//Width of LCD
+#define LCDaddress 0x3F				//Address I2C of LCD
+
+bool soundenable = false;			//Set true to enable playsound
+bool SDfound = false;
+
+LiquidCrystal_I2C lcd(LCDaddress, xlongLCD, ylongLCD);
+
+maindata record[maxquestion];
+
+TMRpcm soundoutput;
+
+File Recorddata;
+
 uint8_t current_reaction = 0, reaction = 0;
-uint8_t question = 1, lastquestion = 0;
+uint8_t questioncount = 0, lastquestioncout = -1;
 
-uint8_t point[maxquestion];
+char startscreen[] = "Hello";
+char endscreen[] = "Thanks for do survey";
 
-char reactionout[6];
 char star = '*';
+char reactionout[6];
 
 state statemode = HALT, laststatemode = ENDREACTION;
 
+/*
+* @brief	Input question to system
+* @note		Copy this line to add more question
+*				+ strcpy(record[i].question, "a");
+*				+ Change "i" for choose no. question
+*				+ Change "a" for change question
+*				+ Number of question must smaller than max question number (maxquestion)
+*/
+
+void inputquestion()
+{
+	strcpy(record[0].question, "a");		//Copy this
+}
+
 void setup() 
 {
+	inputquestion();
+
+	Recorddata = SD.open("record.txt", FILE_WRITE);
+
 	lcd.init();
 	lcd.backlight();
+
+	soundoutput.speakerPin = Speaker_output;
+	soundoutput.setVolume(Speaker_output_volumn);
 
 	pinMode(Button1Pin, INPUT);
 	pinMode(Button2Pin, INPUT);
@@ -49,43 +100,59 @@ void setup()
 }
 
 // the loop function runs over and over again until power down or reset
-void loop() 
+void loop()
 {
-	if (laststatemode != statemode || lastquestion != question || current_reaction != reaction)
+	/*
+	* @brief	Check SD is plug in
+	*/
+	if (!SD.begin(SD_ChipSelectPin))
+	{
+		lcd.setCursor(0, 0);
+		lcd.print("SD not found !");
+		SDfound = true;
+	}
+	/*
+	* @brief	Change state machine
+	* @note		Mode:
+	*			- HALT: Not do anything
+	*			- REACTION: Show question
+	*			- ENDREACTION: 
+	*/
+	if ((laststatemode != statemode || lastquestioncout != questioncount || current_reaction != reaction)&&SDfound)
 	{
 		switch (statemode)
 		{
 		case HALT:
 			lcd.clear();
 			lcd.setCursor(2, 0);
-			lcd.print("Arduinokit.vn");
-			lcd.setCursor(0, 1);
-			lcd.print("Mother facker");
+			lcd.print(startscreen);
 			break;
 		case REACTION:
-			if (current_reaction != reaction || question == 1)
+			if (current_reaction != reaction || questioncount == 1)
 			{
 				for (int i = 1; i <= reaction; i++)
 					strncat(reactionout, &star, 1);
 				lcd.clear();
 				lcd.setCursor(0, 0);
-				lcd.print(question);
+				lcd.print(record[questioncount].question);
 				lcd.setCursor(locateYReactionStar, locateXReactionStar);
 				lcd.print(reactionout);
-				point[question] = reaction;
+				record[questioncount].point = reaction;
 				current_reaction = reaction;
 				memset(reactionout, 0, 5);
 			}
 			break;
 		case ENDREACTION:
+			if (soundenable)
+				soundoutput.play("4.wav");
 			lcd.clear();
-			lcd.print("Chot");
+			lcd.print(endscreen);
 			break;
 		default:
 			break;
 		}
 		laststatemode = statemode;
-		lastquestion = question;
+		lastquestioncout = questioncount;
 	}
 }
 
@@ -107,8 +174,8 @@ void EXTI2()
 		statemode = REACTION;
 		break;
 	case REACTION:
-		++question;
-		if (question <= maxquestion)
+		++questioncount;
+		if (questioncount < maxquestion)
 		{
 			reaction = 0;
 			current_reaction = 7;
@@ -117,7 +184,7 @@ void EXTI2()
 		statemode = ENDREACTION;
 		break;
 	case ENDREACTION:
-		question = 1;
+		questioncount = 1;
 		statemode = HALT;
 		break;
 	default:
